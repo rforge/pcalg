1df9e16a002f4ddbdfb86763b8616f3833b133e8
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-03 17:19:24 +0000

    first cleanup in data/; add timing-info to some tests/
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pcalg@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/test_pc.R
+++ pkg/tests/test_pc.R
@@ -1,5 +1,14 @@
 library(pcalg)
 
+showProc.time <- local({
+    pct <- proc.time()
+    function() { ## CPU elapsed __since last called__
+	ot <- pct ; pct <<- proc.time()
+	cat('Time elapsed: ', (pct - ot)[1:3],'\n')
+    }
+})
+
+
 ##################################################
 ## Standard PC
 ##################################################
@@ -13,7 +22,7 @@ for (ii in 1:nreps) {
   myCPDAG <- dag2cpdag(myDAG)
 
   suffStat <- list(C = cov2cor(trueCov(myDAG)), n = 10^9)
-  indepTest <- gaussCItest 
+  indepTest <- gaussCItest
 
   res <- pc(suffStat, indepTest, p, 0.99)
 
@@ -21,6 +30,7 @@ for (ii in 1:nreps) {
 }
 
 if (!all(check.res)) stop("Test pc wrong: Some CPDAG was not found correctly!")
+showProc.time()
 
 ##################################################
 ## Conservative PC
@@ -36,15 +46,15 @@ g <- randomDAG(p,2/(p-1))
 ## generate n samples of DAG using standard normal error distribution
 ##random data
 
-data(test_conservative_pc_data1)
+load(system.file("external", "test_conservative_pc_data1.rda", package = "pcalg"))
 ## set.seed(67*37)
 ## new.mat <- rmvnorm(n,mean=rep(0,p),sigma=trueCov(g))
-## save(new.mat, file = "/u/kalisch/research/packages/pcalg/data/test_conservative_pc_data1.rda")
-## load(file = "/u/kalisch/research/packages/pcalg/data/test_conservative_pc_data1.rda")
+## save(new.mat, file = "/u/kalisch/research/packages/pcalg/inst/external/test_conservative_pc_data1.rda")
+## load(file = "/u/kalisch/research/packages/pcalg/inst/external/test_conservative_pc_data1.rda")
 
 suffStat.data <- list(C=cor(new.mat1),n=n)
 indepTest.data <- gaussCItest
-  
+
 ##pcAlgo conservative sample
 dag1 <- pc(suffStat.data, indepTest.data, p, alpha=0.005, verbose=FALSE, u2pd="relaxed",conservative=TRUE)
 
@@ -62,12 +72,9 @@ amat.tetrad1[2,4] <- amat.tetrad1[2,6] <- 1
 amat.tetrad1[3,4] <- 1
 amat.tetrad1[4,6] <- amat.tetrad1[4,9] <- 1
 
-correctEst1 <- FALSE
-if (all(dag1.amat==amat.tetrad1)){
-  correctEst1 <- TRUE
-}
-
+correctEst1 <- all(dag1.amat == amat.tetrad1)
 if (!correctEst1) stop("Test sample conservative PC wrong: example 1!")
+showProc.time()
 
 
 ## Example 2
@@ -78,15 +85,15 @@ g <- randomDAG(p,2/(p-1))
 
 ## generate n samples of DAG using standard normal error distribution
 ##random data
-data(test_conservative_pc_data2)
+load(system.file("external", "test_conservative_pc_data2.rda", package = "pcalg"))
 ## set.seed(67*67)
 ## new.mat <- rmvnorm(n,mean=rep(0,p),sigma=trueCov(g))
-## save(new.mat, file = "/u/kalisch/research/packages/pcalg/data/test_conservative_pc_data2.rda")
-## load(file = "/u/kalisch/research/packages/pcalg/data/test_conservative_pc_data2.rda")
+## save(new.mat, file = "/u/kalisch/research/packages/pcalg/inst/external/test_conservative_pc_data2.rda")
+## load(file = "/u/kalisch/research/packages/pcalg/inst/external/test_conservative_pc_data2.rda")
 
 suffStat.data <- list(C=cor(new.mat2),n=n)
 indepTest.data <- gaussCItest
-  
+
 ##pcAlgo conservative sample
 dag2 <- pc(suffStat.data, indepTest.data, p, alpha=0.005, verbose=FALSE, u2pd="relaxed", conservative=TRUE)
 
@@ -108,25 +115,22 @@ amat.tetrad2[7,1] <- 1
 amat.tetrad2[8,5] <- amat.tetrad2[8,12] <- 1
 amat.tetrad2[10,11] <- 1
 
-correctEst2 <- FALSE
-if (all(dag2.amat==amat.tetrad2)){
-  correctEst2 <- TRUE
-}
-
+correctEst2 <- all(dag2.amat == amat.tetrad2)
 if (!correctEst2) stop("Test sample conservative PC wrong: example 2!")
+showProc.time()
 
 
 ## Example 3
 ## new.mat <- read.table("/u/colombo/Diss/RAusw/AFCI/Conservative_algorithms/test_conservative_pc_data3.txt")
-## save(new.mat, file = "/u/kalisch/research/packages/pcalg/data/test_conservative_pc_data3.rda")
-## load(file = "/u/kalisch/research/packages/pcalg/data/test_conservative_pc_data3.rda")
-data(test_conservative_pc_data3)
+## save(new.mat, file = "/u/kalisch/research/packages/pcalg/inst/external/test_conservative_pc_data3.rda")
+## load(file = "/u/kalisch/research/packages/pcalg/inst/external/test_conservative_pc_data3.rda")
+load(system.file("external", "test_conservative_pc_data3.rda", package = "pcalg"))
 
 ##always save the transpose of the matrix to be loaded in Tetrad
 ##write(t(new.mat),file="test_conservative_pc_data3.txt",ncolumns=15)
 suffStat.data <- list(C=cor(new.mat3),n=n)
 indepTest.data <- gaussCItest
-  
+
 ##pcAlgo conservative sample
 dag3 <- pc(suffStat.data, indepTest.data, 15, alpha=0.005, verbose=FALSE, u2pd="relaxed", conservative=TRUE)
 
@@ -151,12 +155,9 @@ amat.tetrad3[13,15] <- 1
 amat.tetrad3[14,7] <- 1
 amat.tetrad3[15,14] <- 1
 
-correctEst3 <- FALSE
-if (all(dag3.amat==amat.tetrad3)){
-  correctEst3 <- TRUE
-}
-
+correctEst3 <- all(dag3.amat == amat.tetrad3)
 if (!correctEst3) stop("Test sample conservative PC wrong: example 3!")
+showProc.time()
 
 
 ##PC algorithm population
@@ -176,12 +177,9 @@ dag5 <- pc(suffStat, indepTest, p, alpha=0.9999, verbose=FALSE, u2pd="relaxed",c
 dag4.amat <- as(dag4@graph,"matrix")
 dag5.amat <- as(dag5@graph,"matrix")
 
-correctEst4 <- FALSE
-if (all(dag4.amat==dag5.amat)){
-  correctEst4 <- TRUE
-}
-
+correctEst4 <- all(dag4.amat == dag5.amat)
 if (!correctEst4) stop("Test population conservative PC wrong: example 4!")
+showProc.time()
 
 
 ## Example 5
@@ -199,12 +197,9 @@ dag7 <- pc(suffStat, indepTest, p, alpha=0.9999, verbose=FALSE, u2pd="relaxed",c
 dag6.amat <- as(dag6@graph,"matrix")
 dag7.amat <- as(dag7@graph,"matrix")
 
-correctEst5 <- FALSE
-if (all(dag6.amat==dag7.amat)){
-  correctEst5 <- TRUE
-}
-
+correctEst5 <- all(dag6.amat == dag7.amat)
 if (!correctEst5) stop("Test population conservative PC wrong: example 5!")
+showProc.time()
 
 ## Example 6
 p <- 35
@@ -221,9 +216,7 @@ dag9 <- pc(suffStat, indepTest, p, alpha=0.9999, verbose=FALSE, u2pd="relaxed",c
 dag8.amat <- as(dag8@graph,"matrix")
 dag9.amat <- as(dag9@graph,"matrix")
 
-correctEst6 <- FALSE
-if (all(dag8.amat==dag9.amat)){
-  correctEst6 <- TRUE
-}
-
+correctEst6 <- all(dag8.amat == dag9.amat)
 if (!correctEst6) stop("Test population conservative PC wrong: example 6!")
+
+showProc.time()
