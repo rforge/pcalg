0923cc391603dfd417b77bbc735410ae229032a1
mkalisch <mkalisch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-24 09:26:45 +0000

    killed some warnings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pcalg@135 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/dev/tests/test_pcorOrder.R
+++ branches/dev/tests/test_pcorOrder.R
@@ -10,7 +10,6 @@ dat <- rmvDAG(1000,g,errDist="normal")
 res1 <- round(pcorOrder(3,4,c(2,1),cor(dat)),8)
 res2 <- round(pcorOrder(3,4,2,cor(dat)),8)
 
-## TODO Markus: this always fails; please fix
 if((res1!=-0.00474175) | (res2!=0.00890934)) {
    stop("Test of pcorOrder: Theoretical result not matched!")
 }
