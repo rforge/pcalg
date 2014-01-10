031b0166e9a75d7c2a61150037688773355de16b
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-10 11:37:22 +0000

    DEBUG output  iff  doExtras
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pcalg@211 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/dev/tests/test_gies.R
+++ branches/dev/tests/test_gies.R
@@ -4,36 +4,40 @@
 #' @author Alain Hauser
 #' $Id$
 
-cat("Testing the causal inference algorithms for interventional data: ")
+cat("Testing the causal inference algorithms for interventional data:\n")
 
 library(pcalg)
 
 load("test_bicscore.rda") # in directory tests/ i.e., typically *not* installed
+str(gauss.data)
 p <- ncol(gauss.data)
 
+(doExtras <- pcalg:::doExtras())
+DBG <- if(doExtras) 2 else 0 # no debugging by default
 ## Tolerance for numerical comparison
-tol <- sqrt(.Machine$double.eps)
+tol <- sqrt(.Machine$double.eps) # = default for all.equal()
 
-fcns <- c(gies, gds)
-fcn.names <- c("GIES", "GDS")
+fcns <- c(GIES = gies, GDS = gds)
 
-for (fi in 1:length(fcns)) {
-  cat(paste(fcn.names[fi], "... ", sep = ""))
-  
+for (nf in names(fcns)) {
+  cat(if(doExtras)"\n\n", nf, if(doExtras)":\n" else ": ... ",
+      if(doExtras) paste0(paste(rep("=", nchar(nf)), collapse=""), "\n"),
+      sep = "")
   for (cpp in c(FALSE, TRUE)) {
     score <- new("GaussL0penIntScore", 
-      targets = gauss.targets, 
-      target.index = gauss.target.index, 
-      data = gauss.data,
-      use.cpp = cpp)
-  
-    # gies(p, gauss.targets, score, DEBUG.LEVEL = 3)
-    est.graph <- fcns[[fi]](p, gauss.targets, score)
-    
-    for (i in 1:p)
-      if (!isTRUE(all.equal(est.graph$essgraph$.in.edges[[i]], gauss.parents[[i]], tolerance = tol)))
+                 targets = gauss.targets, 
+                 target.index = gauss.target.index, 
+                 data = gauss.data,
+                 use.cpp = cpp)
+    est.graph <- fcns[[nf]](p, gauss.targets, score, DEBUG.LEVEL = DBG)
+    for (i in 1:p) {
+      if(doExtras) cat("  use.cpp = ", cpp,"; i = ", i, "\n", sep="")
+      if (!isTRUE(all.equal(est.graph$essgraph$.in.edges[[i]],
+                            gauss.parents[[i]], tolerance = tol)))
         stop("Parents are not estimated correctly.")
+    }
+    print(proc.time())
   }
+  cat("[Ok]\n")
 }
-
-cat("Done.")
+cat(if(doExtras) "\n", "Done.\n")
