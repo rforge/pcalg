c5ee6cd74eda990fe15615e1302f28a5813e4470
mkalisch <mkalisch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-09-08 13:04:13 +0000

    updated copyright statement for lingam
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pcalg@362 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/lingamFuns.R
+++ pkg/R/lingamFuns.R
@@ -1,7 +1,17 @@
 ### Copyright (c) 2013 - 2015  Jonas Peters  [peters@stat.math.ethz.ch]
-### All rights reserved.  See the file COPYING for license terms.
-##  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  ???????
 
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
 ##################################################
 ## exported
 ##################################################
