0fa86d0c238b9542c90530265e930205085c91da
mkalisch <mkalisch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-07-11 07:24:44 +0000

    adapted test_addBgKnowledge to deal with the new argument checkInput
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pcalg@449 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/test_addBgKnowledge.R
+++ pkg/tests/test_addBgKnowledge.R
@@ -5,7 +5,7 @@ set.seed(123)
 g <- pcalg::randomDAG(n = 7, prob = 0.3)
 plot(g)
 cpdag <- dag2cpdag(g)
-plot(cpdag)
+## plot(cpdag)
 cpdag.mat <- t(as(cpdag,"matrix")) ## has correct encoding
 
 ## test 1: using graph, valid
@@ -38,7 +38,8 @@ m7 <- matrix(0, 3,3)
 colnames(m7) <- rownames(m7) <- as.character(1:ncol(m7))
 r7T <- m7
 m7[2,1] <- m7[3,2] <- m7[2,3] <- 1
-r7 <- addBgKnowledge(gInput = m7, x = c(), y = c(), verbose = TRUE)
+r7 <- addBgKnowledge(gInput = m7, x = c(), y = c(), verbose = TRUE,
+                     checkInput = FALSE)
 r7T[2,1] <- r7T[3,2] <- 1
 res[7] <- identical(r7,r7T)
 
@@ -47,7 +48,8 @@ m8 <- matrix(0, 3,3)
 colnames(m8) <- rownames(m8) <- as.character(1:ncol(m8))
 r8T <- m8
 m8[1,2] <- m8[2,3] <- m8[3,1] <- m8[3,2] <- 1
-r8 <- addBgKnowledge(gInput = m8, x = c(), y = c(), verbose = TRUE)
+r8 <- addBgKnowledge(gInput = m8, x = c(), y = c(), verbose = TRUE,
+                     checkInput = FALSE)
 r8T[1,2] <- r8T[3,1] <- r8T[3,2] <- 1
 res[8] <- identical(r8,r8T)
 
@@ -56,7 +58,8 @@ m9 <- matrix(0, 4,4)
 colnames(m9) <- rownames(m9) <- as.character(1:ncol(m9))
 r9T <- m9
 m9[1,2:4] <- m9[2,1] <- m9[3,c(1,2,4)] <- m9[4,1] <- 1
-r9 <- addBgKnowledge(gInput = m9, x = c(), y = c(), verbose = TRUE)
+r9 <- addBgKnowledge(gInput = m9, x = c(), y = c(), verbose = TRUE,
+                     checkInput = FALSE)
 r9T[1,c(2,4)] <- r9T[2,1] <- r9T[3,c(1,2,4)] <- r9T[4,1] <- 1
 res[9] <- identical(r9,r9T)
 
@@ -65,7 +68,8 @@ m10 <- matrix(0, 4,4)
 colnames(m10) <- rownames(m10) <- as.character(1:ncol(m10))
 r10T <- m10
 m10[1,2:4] <- m10[2,c(1,3)] <- m10[3,c(1,4)] <- m10[4,1] <- 1
-r10 <- addBgKnowledge(gInput = m10, x = c(), y = c(), verbose = TRUE)
+r10 <- addBgKnowledge(gInput = m10, x = c(), y = c(), verbose = TRUE,
+                      checkInput = FALSE)
 r10T[1,c(3,4)] <- r10T[2,c(1,3)] <- r10T[3,c(1,4)] <- r10T[4,1] <- 1
 res[10] <- identical(r9,r9T)
 
